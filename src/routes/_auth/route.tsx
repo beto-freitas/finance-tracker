c8723c7d17c4e009c1137b49017aafd22e8e640f
beto-freitas <gilbertofreitas997@gmail.com>
2026-05-31 16:43:59 -0300

    feat(auth): clear query cache when authed user hits guest routes

--- src/routes/_auth/route.tsx
+++ src/routes/_auth/route.tsx
@@ -1,3 +1,4 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { createFileRoute, Navigate, Outlet } from "@tanstack/react-router";
 import { useAuth } from "#/lib/hooks/use-auth";
 
@@ -6,9 +7,14 @@ export const Route = createFileRoute("/_auth")({
 });
 
 function AuthLayout() {
+	const queryClient = useQueryClient();
 	const session = useAuth();
 
 	if (session) {
+		// removes all cached data to prevent data leak between sessions
+		// TODO: move this to a proper location (maybe logout onSuccess)
+		// we won't have any issues with this for now, but it's a bit hacky
+		queryClient.clear();
 		return <Navigate to="/app/dashboard" />;
 	}
 
